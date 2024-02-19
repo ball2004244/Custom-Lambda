1f59a0eda15bc8956fbe391f1a89a0f130dd9d7a
Jack Nguyen <nguyenminhtam7124@gmail.com>
2024-02-18 19:07:04 -0500

    Update invokee_template

--- invokee_template.py
+++ invokee_template.py
@@ -1,4 +1,4 @@
-from config import FUNC_DELIMITER
+from logic.signature import get_return_signature
 import sys
 import pickle
 import base64
@@ -7,16 +7,18 @@ This file serve as a template file to run invokee functions
 #!DO NOT MODIFY THIS FILE!
 '''
 
-def main():
-    return_signature = f'#function-return: {FUNC_DELIMITER}'
+# Call invokee function here
+def invoker() -> None:
     params = sys.argv[1]
     decoded_params = pickle.loads(base64.b64decode(params))
     result = func(*decoded_params)
-    print(f'{return_signature}')
-    print(base64.b64encode(pickle.dumps(result)).decode()) # func's return value
+    # return signature, separate std output with return value
+    print(get_return_signature())
+    print(base64.b64encode(pickle.dumps(result)).decode())  # func's return value
 
-#start-function: KEYPHRASE
-#end-function: KEYPHRASE
+# start-function: KEYPHRASE
+# end-function: KEYPHRASE
+
 
 if __name__ == "__main__":
-    main()
+    invoker()
